e8335a17dfae06ceff2ae04f80f95c598ca63176
judyliu2 <jliu15@stuy.edu>
2018-02-11 09:59:16 -0500

    done

--- main.py
+++ main.py
@@ -17,7 +17,13 @@ draw_line(150, 320,100,340, s,c)
 draw_line(100,340,100,360, s,c)
 draw_line(100,360,150,380, s,c)
 draw_line(150,380,200,350, s,c)
+draw_line(20,0,475,0, s,c)
+draw_line(0,20,0,475, s,c)
 
+draw_line(25,10,40,475, s,c)
+draw_line(20,475,40,200, s,c)
+draw_line(0,0,200,200, s,c)
+draw_line(0,200,200,0, s,c)
 
 c= [218, 69, 104]
 draw_line(100,320,140,322,s,c)
